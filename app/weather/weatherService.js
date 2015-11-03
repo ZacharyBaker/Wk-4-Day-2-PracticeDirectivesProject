c96197e4736b995a76ab33e5edc3e7489620d696
Zach Baker <zach.r.baker1@gmail.com>
2015-11-03 16:43:48 -0700

    the temperature has been confered from kelvin and has been put into the results object

--- app/weather/weatherService.js
+++ app/weather/weatherService.js
@@ -9,9 +9,16 @@ app.service('weatherService', function ($http, $q) {
 			method: 'GET',
 			url: 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&APPID=4a4a83ae19e8d14eb7c142668724f4d8'
 		}).then(function (response) {
-			console.log('this is the temp', response.data.main.temp);
-			console.log('this is the description', response.data.weather[0].description)
-			deferred.resolve();
+
+			// console.log('this is the description', response.data.weather[0].description)
+			var tempInF = Math.floor((response.data.main.temp * (9/5)) - 459.67);
+			// console.log('this is the temp in f', tempInF);
+			var result = {
+				temp: tempInF,
+				description: response.data.weather[0].description
+			}
+			console.log('this is the result object', result);
+			deferred.resolve(result);
 		})
 
 		return deferred.promise;
